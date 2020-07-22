bcaf9ec0706db38e7f350248c4088bb281c7847a
Valentyn Tiron <vt02@Valentyns-MacBook-Pro.local>
2020-07-22 15:37:39 -0400

    redid the server folder and moved everything out

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ const app = express()
 const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+
 const ProductRoutes = require('./routes/productsRoutes')
 const AuthRoutes = require('./routes/authRoutes')
 const OrdersRoutes = require('./routes/ordersRoutes')
+
 const { isAuth } = require('./utils')
 const path = require('path')
 require('dotenv').config()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+
 // Routes
 app.use('/auth/', AuthRoutes)
 app.use('/api/products', ProductRoutes)
 app.use('/api/orders', isAuth, OrdersRoutes)
 
-app.use(express.static('../client/build'))
+app.use(express.static('client/build'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.listen(port, () => {
