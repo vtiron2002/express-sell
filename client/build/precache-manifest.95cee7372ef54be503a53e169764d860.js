4cfc7c2f48fc9576341c7ba3e228a23be44b1e67
Valentyn Tiron <vt02@Valentyns-MacBook-Pro.local>
2020-07-22 15:02:49 -0400

    served build files through backend

---
+++ client/build/precache-manifest.95cee7372ef54be503a53e169764d860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dac52a4fc7a20c7ddc471047f3837d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654a3e98298d7f4ad44c",
+    "url": "/static/css/main.8a548afd.chunk.css"
+  },
+  {
+    "revision": "f358bb3424e03efbeee5",
+    "url": "/static/js/2.69ad7ce5.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.69ad7ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654a3e98298d7f4ad44c",
+    "url": "/static/js/main.fed67345.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -7,21 +7,25 @@ const ProductRoutes = require('./routes/productsRoutes')
 const AuthRoutes = require('./routes/authRoutes')
 const OrdersRoutes = require('./routes/ordersRoutes')
 const { isAuth } = require('./utils')
+const path = require('path')
 require('dotenv').config()
 
 app.use(express.json())
 app.use(morgan('tiny'))
-
 // Routes
 app.use('/auth/', AuthRoutes)
 app.use('/api/products', ProductRoutes)
 app.use('/api/orders', isAuth, OrdersRoutes)
 
+app.use(express.static('../client/build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+})
+
 app.listen(port, () => {
   console.log(`Server started running on port ${port}`)
-  mongoose.connect(
-    process.env.MONGO_URI,
-    { useUnifiedTopology: true, useNewUrlParser: true },
-    () => console.log('Database connected')
+  mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
+    console.log('Database connected')
   )
 })
