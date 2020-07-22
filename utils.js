1c5b7394c34374d0bda4e182e0ea9f947deb0c7f
Valentyn Tiron <vt02@Valentyns-MacBook-Pro.local>
2020-07-22 16:32:21 -0400

    removed auth middleware

--- utils.js
+++ utils.js
@@ -2,19 +2,20 @@ const jwt = require('jsonwebtoken')
 
 const isAuth = (req, res, next) => {
   const token = req.headers.authorization
-  if (token) {
-    const onlyToken = token.slice(7, token.length)
-    jwt.verify(onlyToken, 'BWuSoPsRv_VTvOLT1-JQo2X7cXscucOlMppjp9hMlOc6bA3a1dN4oSiuQgTOveSJPLP8hg', (err, decode) => {
-      if (err) {
-        return res.status(401).send({ message: 'Invalid Token' })
-      }
-      req.user = decode
-      next()
-      return
-    })
-  } else {
-    return res.status(401).send({ message: 'Token is not supplied.' })
-  }
+  next()
+  // if (token) {
+  //   const onlyToken = token.slice(7, token.length)
+  //   jwt.verify(onlyToken, 'BWuSoPsRv_VTvOLT1-JQo2X7cXscucOlMppjp9hMlOc6bA3a1dN4oSiuQgTOveSJPLP8hg', (err, decode) => {
+  //     if (err) {
+  //       return res.status(401).send({ message: 'Invalid Token' })
+  //     }
+  //     req.user = decode
+  //     next()
+  //     return
+  //   })
+  // } else {
+  //   return res.status(401).send({ message: 'Token is not supplied.' })
+  // }
 }
 
 module.exports = { isAuth }
